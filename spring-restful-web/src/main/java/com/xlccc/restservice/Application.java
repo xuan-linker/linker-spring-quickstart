af4ec8ebece5c4d4362b7b5ad3813f1169b3e59c
xuanlccc@gmail.com <xuanlccc@gmail.com>
2020-10-26 21:04:16 +0800

    rest-service

---
+++ spring-restful-web/src/main/java/com/xlccc/restservice/Application.java
@@ -0,0 +1,16 @@
+package com.xlccc.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Linker
+ * @date 2020/10/26 21:02
+ * @description：http://localhost:8080/greeting
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
