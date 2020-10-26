4f8308eb49e84d0c2ebca14d7bf66d4ca992e278
xuanlccc@gmail.com <xuanlccc@gmail.com>
2020-10-26 21:31:48 +0800

    consuming-rest
    随机检索引用

---
+++ spring-consuming-rest/src/main/java/com/xlccc/consumingrest/ConsumingRestApplication.java
@@ -0,0 +1,38 @@
+package com.xlccc.consumingrest;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * uses Spring’s RestTemplate to retrieve a random Spring Boot quotation
+ */
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
+}
