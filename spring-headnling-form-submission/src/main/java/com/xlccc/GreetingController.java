42e84ee45e73111c8bbab05f77583b1cb6ed0dd9
xuanlccc@gmail.com <xuanlccc@gmail.com>
2020-12-07 13:56:11 +0800

    handling-form-subimssion

---
+++ spring-headnling-form-submission/src/main/java/com/xlccc/GreetingController.java
@@ -0,0 +1,27 @@
+package com.xlccc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @author Linker
+ * @date 2020/12/7 13:52
+ * @description：
+ */
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
+}
