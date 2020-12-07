40d58c79e8d399c4e73ddca64849806b24826d36
xuanlccc@gmail.com <xuanlccc@gmail.com>
2020-12-07 14:22:33 +0800

    websocket

---
+++ spring-websocket/src/main/java/com/xlccc/WebSocketApplication.java
@@ -0,0 +1,16 @@
+package com.xlccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Linker
+ * @date 2020/12/7 14:16
+ * @description：https://spring.io/guides/gs/messaging-stomp-websocket/
+ */
+@SpringBootApplication
+public class WebSocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class, args);
+    }
+}
