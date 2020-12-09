b37a35c092ff2e9c0faaf74a47746d01cb47bce1
xuanlccc@gmail.com <xuanlccc@gmail.com>
2020-12-09 13:52:59 +0800

    secuting-web

---
+++ spring-secuting-web/src/main/java/com/xlccc/SecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.xlccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SecuringWebApplication.class, args);
+	}
+
+}
