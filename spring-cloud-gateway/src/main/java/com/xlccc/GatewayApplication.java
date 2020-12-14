027b9738e890328294ad0e97a0b7244ebb423382
xuanlccc@gmail.com <xuanlccc@gmail.com>
2020-12-14 10:33:29 +0800

    spring-cloud-gateway

---
+++ spring-cloud-gateway/src/main/java/com/xlccc/GatewayApplication.java
@@ -0,0 +1,77 @@
+package com.xlccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Linker
+ * @date 2020/12/12 20:21
+ * @description：
+ */
+@RestController
+@EnableConfigurationProperties(GatewayApplication.UriConfiguration.class)
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    /**
+     * creating a simple route
+     *
+     * @param builder
+     * @return
+     */
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p -> p
+//                        .path("/get")
+//                        .filters(f -> f.addRequestHeader("Hello", "World"))
+//                        .uri("http://httpbin.org:80"))
+//                .build();
+//    }
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+        String httpUri = uriConfiguration.getHttpbin();
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(httpUri))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f.hystrix(config -> config
+                                .setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri)).
+                        build();
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+
+    @ConfigurationProperties
+    class UriConfiguration {
+
+        private String httpbin = "http://httpbin.org:80";
+
+        public String getHttpbin() {
+            return httpbin;
+        }
+
+        public void setHttpbin(String httpbin) {
+            this.httpbin = httpbin;
+        }
+    }
+}
